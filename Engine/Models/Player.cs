9eb60f792f91736d2f8b8f6f77c1721fbcd0b840
Adam Young <adamyoung1414@gmail.com>
2025-06-26 19:25:21 -0500

    added ability to complete quest if player has all the items

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -77,8 +77,25 @@ namespace Engine.Models
         public void AddItemToInventory(GameItem item)
         {
             Inventory.Add(item);
-
             OnPropertyChanged(nameof(Weapons));
+        }
+
+        public void RemoveItemFromInventory(GameItem item)
+        {
+            Inventory.Remove(item);
+            OnPropertyChanged(nameof(Quests));
+        }
+
+        public bool HasAllTheseItems(List<ItemQuantity> items)
+        {
+            foreach (ItemQuantity item in items)
+            {
+                if (Inventory.Count(i => i.ItemTypeID == item.ItemID) < item.Quantity)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
